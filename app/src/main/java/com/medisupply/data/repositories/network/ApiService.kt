e8b8f71a0d48a61d65c81845781be0c458b0a881
romeluniandes <r.palominoj@uniandes.edu.co>
2025-09-24 06:52:31 +0200

    Feature | Setup Project

---
+++ app/src/main/java/com/medisupply/data/repositories/network/ApiService.kt
@@ -0,0 +1,5 @@
+package com.medisupply.data.repositories.network
+
+interface ApiService {
+
+}
---
+++ app/src/main/java/com/medisupply/ui/fragments/PedidosFragment.kt
@@ -0,0 +1,42 @@
+package com.medisupply.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.databinding.DataBindingUtil
+import com.medisupply.R
+import com.medisupply.databinding.FragmentPedidosBinding
+
+class PedidosFragment : Fragment() {
+
+    private var _binding: FragmentPedidosBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_pedidos,
+            container,
+            false
+        )
+
+        setupUI()
+
+        return binding.root
+    }
+
+    private fun setupUI() {
+        binding.titleText.text = "Pedidos"
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
