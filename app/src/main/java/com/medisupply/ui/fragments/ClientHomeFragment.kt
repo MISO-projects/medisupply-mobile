836e9c5515198497783771b1e986fc21e5807126
Andrea Fabiana Villamizar Ruiz <af.villamizarr1@uniandes.edu.co>
2025-10-18 15:59:51 -0500

    Se modifica la vista principal para usuarios

--- app/src/main/java/com/medisupply/ui/fragments/ClientHomeFragment.kt
+++ app/src/main/java/com/medisupply/ui/fragments/ClientHomeFragment.kt
@@ -1,10 +1,13 @@
 package com.medisupply.ui.fragments
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.medisupply.R
 
 class ClientHomeFragment : Fragment() {
@@ -14,5 +17,34 @@ class ClientHomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_client_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val logoutText = view.findViewById<TextView>(R.id.logout_text)
+        logoutText.setOnClickListener {
+            logout()
+        }
+    }
+
+    private fun logout() {
+        // Borrar datos de sesión y preferencias
+        clearSessionData()
+
+        // Navegar al LoginFragment
+        val navController = findNavController()
+        navController.navigate(R.id.action_clientHomeFragment_to_loginFragment)
+    }
+
+    private fun clearSessionData() {
+        // Obtener referencia a las SharedPreferences
+        val sharedPreferences = requireContext().getSharedPreferences("MyAppPreferences", Context.MODE_PRIVATE)
+
+        // Borrar todos los datos de sesión
+        with(sharedPreferences.edit()) {
+            clear()
+            apply()
+        }
     }
 }
